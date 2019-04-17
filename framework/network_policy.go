b3ee2815b0451f59525fb09690509badd75eabd4
Fahim Abrar <fahimabrar@appscode.com>
2019-04-17 15:25:49 +0600

    Add network policy

---
+++ framework/network_policy.go
@@ -0,0 +1,40 @@
+package framework
+
+import (
+	v1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (i *lbInvocation) GetNetworkPolicyObject(labels map[string]string) *v1.NetworkPolicy {
+	return &v1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "network-policy",
+			Namespace: i.Namespace(),
+		},
+		Spec: v1.NetworkPolicySpec{
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			PolicyTypes: []v1.PolicyType{
+				v1.PolicyTypeIngress,
+			},
+			Ingress: []v1.NetworkPolicyIngressRule{
+				{
+					From: []v1.NetworkPolicyPeer{
+						{
+							IPBlock: &v1.IPBlock{
+								CIDR: "192.168.0.0/16",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (i *lbInvocation) CreateNetworkPolicy(np *v1.NetworkPolicy) error {
+	_, err := i.kubeClient.NetworkingV1().NetworkPolicies(i.Namespace()).Create(np)
+
+	return err
+}

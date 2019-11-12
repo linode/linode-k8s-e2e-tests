dd5394fc3bdc5163782408f970e8e39906b2e960
John Schaeffer <john@schaeffer.io>
2019-11-12 15:18:01 -0500

    Removed || true from shell calls
    This was silly. I don't even know why it was there.

--- scripts/helm-delete.sh
+++ scripts/helm-delete.sh
@@ -2,7 +2,7 @@
 
 CLUSTERCONF=$1
 
-kubectl delete serviceaccount tiller --kubeconfig=${CLUSTERCONF} || true
-kubectl delete clusterrolebinding tiller-cluster-rule --kubeconfig=${CLUSTERCONF} || true
+kubectl delete serviceaccount tiller --kubeconfig=${CLUSTERCONF}
+kubectl delete clusterrolebinding tiller-cluster-rule --kubeconfig=${CLUSTERCONF}
 
 helm reset --force --kubeconfig=${CLUSTERCONF}
--- scripts/helm-init.sh
+++ scripts/helm-init.sh
@@ -2,7 +2,7 @@
 set -exou pipefail
 
 CLUSTERCONF=$1
-kubectl create serviceaccount --namespace kube-system tiller --kubeconfig=${CLUSTERCONF} || true
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller --kubeconfig=${CLUSTERCONF} || true
+kubectl create serviceaccount --namespace kube-system tiller --kubeconfig=${CLUSTERCONF}
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller --kubeconfig=${CLUSTERCONF}
 
-helm init --service-account=tiller --kubeconfig=${CLUSTERCONF} || true
+helm init --service-account=tiller --kubeconfig=${CLUSTERCONF}

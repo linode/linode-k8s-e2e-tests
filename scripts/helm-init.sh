d85fb3bf0675127016780da73567b377dd9fb2b1
thorner <thorner@linode.com>
2020-11-17 17:04:13 -0500

    Update helm installation of Wordpress to helm3 syntax and new stable repo

--- scripts/helm-init.sh
+++ scripts/helm-init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -exou pipefail
 
-CLUSTERCONF=$1
-kubectl create serviceaccount --namespace kube-system tiller --kubeconfig=${CLUSTERCONF}
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller --kubeconfig=${CLUSTERCONF}
-
-helm init --service-account=tiller --kubeconfig=${CLUSTERCONF}
+helm repo add bitnami https://charts.bitnami.com/bitnami
